7af865861f4afee414acb222bd3daa2305227cf6
Paulo García <psebastian21@gmail.com>
2025-07-23 22:55:03 -0500

    useRouteLoaderData instead of useLoaderData to share loaders across pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -40,9 +40,13 @@ const router = createBrowserRouter([
             {
                 path: "events", element: <EventsRootLayout/>, children: [
                     {index: true, element: <EventsPage/>, loader: eventsLoader},
-                    {path: ":eventId", element: <EventDetailPage/>, loader: eventDetailLoader},
+                    {path: ":eventId", id: "event-detail", loader: eventDetailLoader, children: [
+                            {index: true, element: <EventDetailPage/>},
+                            {path: "edit", element: <EditEventPage/>},
+                        ]},
+
                     {path: "new", element: <NewEventPage/>},
-                    {path: ":eventId/edit", element: <EditEventPage/>},
+
                 ]
             },
 
